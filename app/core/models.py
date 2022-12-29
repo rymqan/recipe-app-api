511569eea71e53dc078c45daf457a19002114ab6
rymqan <rymqan1203@gmail.com>
2022-12-29 15:43:21 +0600

    Fixed problems with whitespaces and newlines in models and test_models

--- app/core/models.py
+++ app/core/models.py
@@ -11,7 +11,7 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     """Manager model"""
-    
+
     def create_user(self, email, password=None, **extra_fields):
         """Create user"""
         if not email:
@@ -19,17 +19,18 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        
+
         return user
-        
+
     def create_superuser(self, email, password):
         """Create superuser"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-        
+
         return user
+    
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User model"""
@@ -37,8 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
-    
